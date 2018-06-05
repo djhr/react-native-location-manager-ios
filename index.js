2488dd10e7a4d2b4b2fd0b146d0b549ccda0e674
Daniel Rosa <djhrosa@gmail.com>
2018-06-06 00:35:52 +0200

    Copyright notice

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2018-present, Daniel Rosa.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { NativeModules, NativeEventEmitter } from 'react-native';
 
 const NativeModule = NativeModules.LocationManagerIOS;
